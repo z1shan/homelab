f90b793f455d304ae47614d768d6f2882ce90590
z1shan <xshanm@gmail.com>
2026-09-01 11:30:42 +0000

    Auto-update configs - 2026-09-01 11:30:42

--- backup-configs.sh
+++ backup-configs.sh
@@ -7,7 +7,7 @@ REPO="/home/zishan/github/homelab"
 cp /mnt/immichssf/docker-compose.yml  "$REPO/immich/immich-docker-compose.yml"
 cp /home/zishan/glance/config/glance.yml "$REPO/glance/glance.yml"
 cp /home/zishan/glance/config/home.yml "$REPO/glance/home.yml"
-cp /home/zishan/glance/config/docker-compose.yml "$REPO/glance/docker-compose.yml"
+cp /home/zishan/glance/docker-compose.yml "$REPO/glance/docker-compose.yml"
 cp /home/nginxpm/docker-compose.yml "$REPO/nginxpm/docker-compose.yml"
 cp /home/beszel/docker-compose.yml "$REPO/beszel/docker-compose.yml"
 
